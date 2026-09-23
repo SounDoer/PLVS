03e3bbcd99420e6d84d14a3bfb832a549eb70322
SounDoer <xichenphoto@gmail.com>
2026-09-23 13:54:50 +0800

    test(persistence): classify instance identity reader

--- src/persistence/initialStateContract.test.js
+++ src/persistence/initialStateContract.test.js
@@ -27,6 +27,7 @@ const READERS = {
   ],
   "hooks/useDockMode.js": ["dockState"],
   "hooks/useInstanceIdentity.js": ["agentControl"],
+  "hooks/useTray.js": ["instanceId"],
   "hooks/useAutostart.js": ["globalPreferences", "multiInstancePersistence"],
   "ipc/capturePrefs.js": ["captureDeviceId", "multiInstancePersistence"],
   "lib/clearShortcutPrefs.js": ["globalPreferences", "multiInstancePersistence"],
